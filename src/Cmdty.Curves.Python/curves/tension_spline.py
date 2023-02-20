17034493d8dec1b1e989901461c2b6e78ac47d24
Jake Fowler <fowlerjake@hotmail.com>
2023-02-20 13:52:46 +0530

    Added skeleton for tension spline Python code.

--- src/Cmdty.Curves.Python/curves/tension_spline.py
+++ src/Cmdty.Curves.Python/curves/tension_spline.py
@@ -1,9 +1,9 @@
-# Copyright(c) 2019 Jake Fowler
+# Copyright(c) 2023 Jake Fowler
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
 # files (the "Software"), to deal in the Software without
-# restriction, including without limitation the rights to use, 
+# restriction, including without limitation the rights to use,
 # copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following
@@ -12,29 +12,27 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-import platform
+import pandas as pd
+import numpy as np
+from typing import Optional, Callable, Union
+from curves._common import ContractsType
 
-os = platform.system()
-# On non-Windows platform try to load Core CLR, rather than the default behaviour which is to load Mono.
-if os != 'Windows':
-    from pythonnet import load
 
-    try:
-        load('coreclr')
-    except:
-        print('Could not load Core CLR runtime, on non-Windows OS, so falling back to Mono.')
+def tension_spline(contracts: Union[ContractsType, pd.Series],
+                    freq: str,
+                    tension: float,
+                    mult_season_adjust: Optional[Callable[[pd.Period], float]] = None,
+                    add_season_adjust: Optional[Callable[[pd.Period], float]] = None,
+                    average_weight: Optional[Callable[[pd.Period], float]] = None) -> pd.Series:
+    return pd.Series()
 
-from curves.bootstrap import bootstrap_contracts
-from curves.spline import max_smooth_interp
-from curves.tension_spline import tension_spline
-from curves._common import FREQ_TO_PERIOD_TYPE
-from curves.__version__ import __version__
+
