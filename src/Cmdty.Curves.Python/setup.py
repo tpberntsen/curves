bccc031dee2f90c3de997f1b556a58fdfd22e778
Jake Fowler <fowlerjake@hotmail.com>
2024-02-15 08:10:15 +0000

    Pythonnet and Pandas forward compatibility with patch versions.

--- src/Cmdty.Curves.Python/setup.py
+++ src/Cmdty.Curves.Python/setup.py
@@ -65,8 +65,8 @@ setuptools.setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     install_requires=[
-        'pythonnet>=3.0.1, <=3.0.3',
-        'pandas>=1.0.0, <=2.2.0'
+        'pythonnet>=3.0.1, <3.1.0a0',
+        'pandas>=1.0.0, <2.3.0a0'
         ],
     python_requires='>=3.7, <3.13',
     package_data={'curves' : [
